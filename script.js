3cf2af056733c1d65cca1b1ffaf2aea157fe5fbf
cdnau <runpenrun123@gmail.com>
2024-12-19 14:07:52 +0700

    Scroll to main button

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-// ฟังก์ชั่นสำหรับการสลับธีม
 function toggleTheme() {
-    // เพิ่มหรือลบคลาส 'dark-theme' จาก <body>
     document.body.classList.toggle('dark-theme');
+}
+
+window.addEventListener("scroll", function () {
+    const backToTopButton = document.getElementById("backToTop");
+    if (window.scrollY > 300) {
+        backToTopButton.classList.add("show"); 
+    } else {
+        backToTopButton.classList.remove("show"); 
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth", 
+    });
 }
